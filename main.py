f266e24d2edf2eaeb2dc423791740fbb5ce742ae
Emre Doğru <60527111+emredgr@users.noreply.github.com>
2023-07-08 02:24:48 +0300

    Updated main.py for added fetch class.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from api_connection.connectAPI import ConnectBinance
 from configurations.read_config import ReadConfiguration
+from extract.fetch import FetchData
+from pandas import DataFrame
 import logging
+
 
 class Manager(object):
     """Class for managing the data fetching process."""
@@ -10,7 +13,7 @@ class Manager(object):
         configurations: dict= config_reader.readConfig()
 
         connection_data = configurations["Connection"]
-        self.time_interval_value = configurations["Time_Interval"][True]
+        self.time_interval = configurations["Time_Interval"][True]
         self.start_date = configurations["Date_Range"]["start_date"]
         self.stop_date = configurations["Date_Range"]["stop_date"]
         
@@ -18,8 +21,15 @@ class Manager(object):
         connection = ConnectBinance(config_binance_data=connection_data)
         self.client = connection.connect()
         
+    def fetchData(self):
+        fetcher = FetchData(self.client)
+        dataframe: DataFrame= fetcher.fetchData(coin= "BTC", time_interval= self.time_interval,
+                          start_date= self.start_date, stop_date= self.stop_date)
+        logging.info(f" Fetched data count : {dataframe.shape}")
+        
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s", datefmt="%d-%m-%Y %H:%M:%S")
     
-    trading_bot=Manager()
+    manager=Manager()
+    manager.fetchData()
