5cac823ce9676509bbdb95e123e09df44a94a7ad
Emre Doğru <60527111+emredgr@users.noreply.github.com>
2023-07-09 23:01:17 +0300

    fetch.py updated.

--- extract/fetch.py
+++ extract/fetch.py
@@ -1,26 +1,43 @@
 from binance.client import Client
-from pandas import DataFrame,to_datetime
+from pandas import DataFrame
 from datetime import datetime
 import logging
 
 class FetchData(object):
 
-    def __init__(self, client: Client) -> None:
+    def __init__(self, client: Client, coin: str, time_interval: str,
+                 start_date: str, stop_date: str) -> None:
+        self.dataframe = DataFrame()
+
         self.client: Client = client
+        self.coin = coin
+        self.time_interval = time_interval
+        self.start_date = start_date
+        self.stop_date = stop_date
         
-    def fetchData(self, coin: str, start_date: str,
-                        stop_date: str, time_interval: str) -> DataFrame:
+    def fetchDataFromBinance(self):
         
-        coin: str = f"{coin}USDT"
-        start_date: str = datetime.strptime(start_date, "%Y-%m-%d").strftime("%Y-%m-%d %H:%M:%S")
-        stop_date: str =  datetime.strptime(stop_date, "%Y-%m-%d").strftime("%Y-%m-%d %H:%M:%S")
+        coin: str = f"{self.coin}USDT"
+        start_date: str = datetime.strptime(self.start_date, "%Y-%m-%d").strftime("%Y-%m-%d")
+        stop_date: str =  datetime.strptime(self.stop_date, "%Y-%m-%d").strftime("%Y-%m-%d")
 
-        logging.info(f"Fetching data for {coin} coin within the date range of {start_date} to {stop_date}.")
 
-        data = self.client.get_historical_klines(symbol= coin, interval= time_interval,
+        
+
+        data = self.client.get_historical_klines(symbol= coin, interval= self.time_interval,
                                                  start_str= start_date, end_str= stop_date)
         
-        df: DataFrame = DataFrame(data, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume', 'close_time', 'quote_asset_volume', 'number_of_trades', 'taker_buy_base_asset_volume', 'taker_buy_quote_asset_volume', 'ignore'])
+        self.dataframe: DataFrame = DataFrame(data, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume', 'close_time', 'quote_asset_volume', 'number_of_trades', 'taker_buy_base_asset_volume', 'taker_buy_quote_asset_volume', 'ignore'])
         
-        return df
-                    
+        logging.info(f"Data fetched for {self.coin} coin within the date range of {start_date} to {stop_date}.")
+        logging.info(f" Fetched data count : {self.dataframe.shape[0]}")
+
+
+    def fetchData(self):
+        self.fetchDataFromBinance()
+    
+        return self.dataframe
+    
+
+        
+        
